56f0ecab18feba211ddd4f1df1c36077aee4d1af
nikobatzi1990 <nicole.boci@gmail.com>
2023-03-29 20:58:38 +0900

    feat: create Navbar component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,13 @@
 // import './App.css';
 import Header from './Header';
-import Button  from './Button';
+import Navbar from './Navbar';
+import Button from './Button';
 import Input from './Input';
 
 function App() {
   return (
     <div className="App">
-
+      
     </div>
   );
 }
