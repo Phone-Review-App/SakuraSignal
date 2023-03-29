81885ae3a5e878dfc81d56269b03cd033403950c
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-03-29 19:19:18 +0900

    console.loging

--- db/index.js
+++ db/index.js
@@ -1,5 +1,5 @@
 const knex = require('knex');
 const knexConfigs = require("../knexfile");
 //const knexConfig = process.env.NODE_ENV === 'production' ? 'production' : 'development';
-
+console.log('🍕', NODE_ENV);
 module.exports = knex(knexConfigs[NODE_ENV]);
