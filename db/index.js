7d7bdb02de266ebeccec76b90268c84e1d8d5660
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-03-29 19:26:00 +0900

    test

--- db/index.js
+++ db/index.js
@@ -2,5 +2,5 @@ const knex = require('knex');
 require('dotenv').config();
 const knexConfigs = require("../knexfile");
 //const knexConfig = process.env.NODE_ENV === 'production' ? 'production' : 'development';
-console.log('🍕', NODE_ENV);
+console.log('🍕', knexConfigs);
 module.exports = knex(knexConfigs[process.env.NODE_ENV]);
