fa6cba828a198fb4ac7ae7ce7ad0a38eb4953f24
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-03-29 19:15:15 +0900

    TEST: development

--- knexfile.js
+++ knexfile.js
@@ -10,7 +10,7 @@ require("dotenv").config({
 module.exports = {
   development: {
     client: "pg",
-    connection: process.env.DB_URL || {
+    connection: process.env.DATABASE_URL || {
       host: process.env.DB_HOST || "127.0.0.1",
       port: process.env.DB_PORT || 5432,
       database: process.env.DB_NAME,
