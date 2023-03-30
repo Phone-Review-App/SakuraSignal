083b811f50cf272995f05318cfbabc31ecbae788
nikobatzi1990 <nicole.boci@gmail.com>
2023-03-30 14:00:15 +0900

    fix: remove line importing React DOM twice

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
