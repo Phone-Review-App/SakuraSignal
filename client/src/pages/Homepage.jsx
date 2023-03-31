b5dbdf8848a5dbed64f92d901ceaee742416e20b
Nicole <103890618+nikobatzi1990@users.noreply.github.com>
2023-03-31 20:30:05 +0900

    27 handle routing for pages (#53)
    * feat: create different files for pages
    
    * feat: handle routing pages from Homepage
    
    * fix: remove line causing two copies of navbar
    
    * fix: remove line importing React DOM twice
    
    * wip: create review input page
    
    * chore: refactor to make buttons into routers
    
    * wip: make buttons on homepage route to profile pages
    
    * fix: remove unneeded characters from routes
    
    * fix: remove unnecessary console log

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 import './Homepage.css';
 import Card from '../components/Card'
 import DropdownMenu from '../components/DropdownMenu'; // Remove me after test
@@ -14,8 +15,10 @@ const Homepage = () => {
 
   async function getProviders() {
     const fetchedProviders = await axios.get('/api/providers');
-    setProviders(fetchedProviders.data)
+    setProviders(fetchedProviders.data);
   }
+
+  const navigate = useNavigate();
 
   return (
     <>
@@ -30,6 +33,7 @@ const Homepage = () => {
             english_support={provider.english_support}
             site_url={provider.site_url}
             description={provider.description}
+              onClick={() => navigate('/' + provider.name)}
           />
         )
       })
