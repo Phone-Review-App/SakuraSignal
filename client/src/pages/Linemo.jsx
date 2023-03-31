a019eee08d7bc107f36500844492a580bd6a27c9
nikobatzi1990 <nicole.boci@gmail.com>
2023-03-31 23:34:11 +0900

    feat: hydrate provider profile pages with data

--- client/src/pages/Linemo.jsx
+++ client/src/pages/Linemo.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import axios from "axios";
+import ReviewCard from "../components/ReviewCard";
+import ProviderInfo from "../components/ProviderInfo";
+import AvgScores from "../components/AvgScores";
+import "./Provider.css";
 
 const Linemo = () => {
-    return (
-        <div></div>
-    );
+    const [provider, setProvider] = useState([{},[]]);
+
+    useEffect(() => {
+      getProvider();
+    },)
+  
+    async function getProvider() {
+  
+      const fetchedProvider = await axios.get('/api/provider/6'); 
+      setProvider(fetchedProvider.data)
+    }
+      return (
+          <div>
+            <ProviderInfo provider={provider[0]} />
+            <div className="main-content">
+              <AvgScores scores={provider[0]} />
+              <ReviewCard reviews={provider[1]}></ReviewCard>
+            </div>
+          </div>
+      );
 }
 
 export default Linemo;
