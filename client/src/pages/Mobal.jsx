bdfc6ede0df95f51b4b838647a480c4e13a97fb6
TaylorC19 <96517792+TaylorC19@users.noreply.github.com>
2023-03-31 13:32:09 +0900

    feat: import provider info to mobal.jsx

--- client/src/pages/Mobal.jsx
+++ client/src/pages/Mobal.jsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
+import axios from "axios";
 
 const Mobal = () => {
+  const [provider, setProvider] = useState([]);
+
+  useEffect(() => {
+    getProviders();
+  },)
+
+  async function getProvider1() {
+    const fetchedProvider = await axios.get('/api/provider/1');
+    setProvider(fetchedProvider.data)
+  }
     return (
         <div></div>
     );
