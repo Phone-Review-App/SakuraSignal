203df25f296a487943e4b559d6724a310b6215d2
Nicole <103890618+nikobatzi1990@users.noreply.github.com>
2023-03-31 21:22:51 +0900

    27 handle routing for pages (#55)
    * feat: create different files for pages
    
    * feat: handle routing pages from Homepage
    
    * fix: remove line causing two copies of navbar
    
    * fix: remove line importing React DOM twice
    
    * wip: create review input page
    
    * chore: refactor to make buttons into routers
    
    * wip: make buttons on homepage route to profile pages
    
    * fix: remove unneeded characters from routes
    
    * fix: remove unnecessary console log
    
    * fix: delete unnecessary Router page component
    
    * fix: delete unnecessary Layout page component
    
    * fix: delete file
    
    * fix: accidentally deleted lines

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,7 @@
 import './index.css';
 import React from 'react';
-import './index.css';
-import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Form from "./pages/Form";
 import AU from "./pages/AU";
@@ -14,11 +13,55 @@ import Rakuten from "./pages/Rakuten";
 import Softbank from "./pages/Softbank";
 import UQ from "./pages/UQ";
 import Ymobile from "./pages/Ymobile";
-import Layout from './pages/Layout';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />
+  },
+  {
+    path: "/Form",
+    element: <Form />
+  },
+  {
+    path: "/AU",
+    element: <AU />
+  },
+  {
+    path: "/Docomo",
+    element: <Docomo />
+  },
+  {
+    path: "/GTN Mobile",
+    element: <GTN />
+  },
+  {
+    path: "/Linemo",
+    element: <Linemo />
+  },
+  {
+    path: "/Mobal",
+    element: <Mobal />
+  },
+  {
+    path: "/Rakuten Mobile",
+    element: <Rakuten />
+  },
+  {
+    path: "/Softbank",
+    element: <Softbank/>
+  },
+  {
+    path: "/UQ Mobile",
+    element: <UQ />
+  },
+  {
+    path: "/Y!mobile",
+    element: <Ymobile />
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <RouterProvider router={ router } />
 );
-
-
