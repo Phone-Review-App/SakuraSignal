7259cdb8a97376b46907883e5ebb3a9af1ff41c0
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2023-04-04 19:13:24 +0800

    final clearup comment component

--- client/src/components/DropdownMenu.jsx
+++ client/src/components/DropdownMenu.jsx
@@ -21,17 +21,9 @@ function DropdownMenu ( props ) {
 
 
   const [selected, setSelected ] = useState(options[0].value);
-  //const [HasChangedValue, setHasChangedValue ] = useState(false);
-  
-  useEffect (()=> {
-  // fixme: How to intervein "selected" so it is targeted
-  },[selected]);
-
-  // const handleChangedValue = (event) => {
-  // } 
 
   const handleChange = (event) => {
-  // console.log(event.target.value);
+  
   setSelected(event.target.value);
   let company=event.target.value;
   console.log("company:",company);
@@ -66,39 +58,7 @@ function DropdownMenu ( props ) {
   );
 };
 
-// function DropdownMenuX ( props ){
 
-//     const [ selectOption, setSelectOption ] = useState('AU');
-
-//     useEffect(() => {
-
-//     })
-
-//     const { htmlFor, labelName, selectClassName, selectName, selectid, selected, onChange, onClick } = props;
-//     return (
-//     <>
-//     <label htmlFor={htmlFor} onChange={onChange}>{labelName}</label>
-//     <select 
-//     options={options}
-//         className={selectClassName} 
-//         name={selectName} 
-//         id= {selectid} 
-//         value={selected}
-//     >
-//         <option value="AU">AU</option>
-//         <option value="Docomo">Docomo</option>
-//         <option value="GTN">GTN</option>
-//         <option value="Linemo">Linemo</option>
-//         <option value="Mobal">Mobal</option>
-//         <option value="Rakuten">Rakuten</option>
-//         <option value="Softbank">Softbank</option>
-//         <option value="UQ">UQ</option>
-//         <option value="Ymobile">Ymobile</option>
-//     </select>
-//     <Button text="Submit" onClick={onClick}/>
-//     </>
-//     )
-// }
 
 DropdownMenu.defaultProps ={
     className: "",
