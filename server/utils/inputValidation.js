403093d0665849e670b0fa71bd773d63de5eda7d
kappanjoe <6261485+kappanjoe@users.noreply.github.com>
2023-04-05 19:31:52 +0900

    feat: create server/utils/inputValidation.js

---
+++ server/utils/inputValidation.js
@@ -0,0 +1,6 @@
+function validEmail(input) {
+    return /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+        .test(input); // Regex version of HTML5 spec
+}
+
+module.exports = { validEmail }
