50c959457b993bcefa089e8702dce5744dc24f04
Lak Gill <LakGill88@gmail.com>
2023-12-06 01:23:39 +0900

    109 name (#122)
    * feat: add GoogleFont to Header
    
    * refactor: change all of the usages of teal to pink to match the new theme
    
    * refactor: change the hover color to a darker pink
    
    * refactor: change the footer to match the new name

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,8 +5,8 @@ const Header = (props) => {
   const { className, text, secondary_text } = props;
   return (
     <div className={ className }>
-      <h1>{ text }</h1>
-      <h2> { secondary_text }</h2>
+      <h1 className='title'>{ text }</h1>
+      <h2 className='subtitle'> { secondary_text }</h2>
     </div>
   )
 };
