2374f0cfb80cd0770a6c2646f36b352613a47f19
SirrorsMoore1975 <SirrorsMoore1975@github.com>
2023-12-15 02:43:41 +0800

    adding hero icons wip

---
+++ client/src/components/icons/ArrowLeftCircle.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {ArrowLeftCircleIcon as ArrowLeftCircleOutline} from "@heroicons/react/20/outline";
+import {ArrowLeftCircleIcon as ArrowLeftCircleSolid} from "@heroicons/react/20/solid";
+import "../../styles/icons/ArrowLeftCircle.css";
+
+export default function ArrowLeftCircle({handleHover}){
+    return (
+        <>
+        <div>
+            
+        </div>
+        </>
+    )
+}
---
+++ client/src/components/icons/ArrowRightCircle.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {ArrowRightCircleIcon as ArrowRightCircleOutline} from "@heroicons/react/20/outline";
+import {ArrowRightCircleIcon as ArrowRightCircleSolid} from "@heroicons/react/20/solid";
+import "../../styles/icons/ArrowRightCircle.css";
+
+export default function ArrowRightCircle({handleHover}){
+    return (
+        <>
+        <div>
+            
+        </div>
+        </>
+    )
+}
